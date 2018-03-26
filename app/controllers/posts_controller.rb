d484339870393eaa392f1046716e08353f05c481
Igor Kolomiyets <ikolomiyets@gmail.com>
2018-03-26 12:31:10 +0100

    Added model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @header = "Sample Post One"
+    @content = "This is sample text"
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
