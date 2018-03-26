82f7330c5c3d1b235021283abc2cd3794bf312d8
Igor Kolomiyets <ikolomiyets@gmail.com>
2018-03-26 16:22:01 +0100

    Added the database stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,50 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+
     @header = "Sample Post One"
     @content = "This is sample text"
   end
 
   def new
-
+    @post = Post.new
+    @categories = Category.all
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, :notice => "Your post has been saved"
+    else
+      render "new"
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
+    @categories = Category.all
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to post_path, :notice => "Your post has been updated"
+    else
+      render "edit"
+    end
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
   end
 end
